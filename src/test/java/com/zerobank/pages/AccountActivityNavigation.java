8b2c3a3ece20d78432c91b721551fc1cf4561a27
Ali Kocaaga <alikocaaga@gmail.com>
2019-12-31 00:17:48 -0500

    zerobank

--- src/test/java/com/zerobank/pages/AccountActivityNavigation.java
+++ src/test/java/com/zerobank/pages/AccountActivityNavigation.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
+
 import java.util.List;
 
 public class AccountActivityNavigation extends BasePage {
